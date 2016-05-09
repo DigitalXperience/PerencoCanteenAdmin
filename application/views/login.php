30c54b4773001bef5d286b4e4188f06c3f608ac1
Patient Assontia <assontia@gmail.com>
2016-05-09 07:39:13 +0100

    Javascript authentication
    Javascript authentication with variables

--- application/views/login.php
+++ application/views/login.php
@@ -12,8 +12,8 @@
 		<img src="<?php echo base_url(); ?>/assets/images/perenco2.png" class="ri" style="height: 120px;" />
 		<h1 id="mtsg">Staff Canteen Administration</h1>
 		<form class="form">
-			<input type="text" placeholder="Username">
-			<input type="password" placeholder="Password">
+			<input type="text" id="username" placeholder="Username">
+			<input type="password" id="password" placeholder="Password">
 			<button type="submit" id="login-button">Login</button>
 		</form>
 	</div>
@@ -30,8 +30,39 @@
 		<li></li>
 	</ul>
 </div>
-    <script src='<?php echo base_url(); ?>/assets/js/jquery.min.js'></script>
-
-        <script src="<?php echo base_url(); ?>/assets/js/index.js"></script>
-  </body>
+<script src='<?php echo base_url(); ?>/assets/js/jquery.min.js'></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<!--<script src="<?php echo base_url(); ?>/assets/js/index.js"></script>-->
+</body>
+<script>
+$("#username, #password").keyup(function(event){
+	$('#mtsg').html('Staff Canteen Administration');
+});
+ $("#login-button").click(function(event){
+	event.preventDefault();
+	var login = 'junior';
+	var password = 'ndjaya';
+	
+	if($("#password").val() == password && login == $("#username").val()) {
+		$('form').fadeOut(500);
+		$('.wrapper').addClass('form-success');
+		setTimeout(function(){
+			$(location).attr('href', '<?php echo base_url(); ?>/dashboard')
+			return false;
+		}, 2000);
+	} else {
+		if(login != $("#username").val()) {
+			$('#mtsg').html('Login Incorrect...');
+			$('#mtsg').addClass('error');
+			$('#username').addClass('error');
+		}
+		if(password != $("#password").val()) {
+			$('#mtsg').html('Mot de passe Incorrect...');
+			$('#mtsg').addClass('error');
+			$('#password').addClass('error');
+		}
+	}
+	
+});
+</script>
 </html>
