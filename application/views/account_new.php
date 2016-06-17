87ad64558b07d4b33fbf3768efdc8006d2fa47b7
Patient Assontia <assontia@gmail.com>
2016-06-17 02:13:55 +0100

    MAJ design ajouter un compte
    Quelques mises à jour de la forme

--- application/views/account_new.php
+++ application/views/account_new.php
@@ -6,246 +6,89 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
           <h1>
-            Dashboard
-            <small>Control panel</small>
+             <?php echo $title; ?>
           </h1>
           <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Dashboard</li>
+            <li><a href="#"><i class="fa fa-dashboard"></i> Accueil</a></li>
+            <li class="active">Ajouter un compte</li>
           </ol>
         </section>
 
         <!-- Main content -->
         <section class="content">
           <?php if(isset($alert)) echo $alert; ?>
-			<div class="row">
-			<div class="col-md-6">
+			
+			
               <!-- general form elements -->
               <div class="box box-primary">
                 <div class="box-header with-border">
-                  <h3 class="box-title">Créer les comptes des utilisateurs</h3>
+                  <h3 class="box-title">Créer un compte utilisateur</h3>
                 </div><!-- /.box-header -->
                 <!-- form start -->
                 <form role="form" method="POST" action="<?php echo base_url(); ?>index.php/account/newaccount">
                   <div class="box-body">
-					<div class="form-group">
-                      <label>Selectionner utilisateur</label>
-                      <select class="form-control" name="id_user">
-						<?php foreach($users as $user) { ?>
-                        <option value="<?php echo $user->id_user ?>"><?php echo $user->lastname . ' ' . $user->firstname; ?></option>
-                        <?php } ?>
-                      </select>
-                    </div>
-                    <div class="form-group">
-                      <label for="PIN">PIN</label>
-                      <input type="text" class="form-control" id="PIN" name="PIN" value="<?php echo $pin; ?>">
-                    </div>
-					<div class="form-group">
-                      <label>Status</label>
-                      <select class="form-control" name="blocked">
-                        <option value="0">Activé</option>
-                        <option value="1">Bloqué</option>
-                      </select>
-                    </div>
+					<div class="col-md-6">
+						<div class="form-group">
+						  <label>Selectionner utilisateur</label>
+						  <select class="form-control" name="id_user" id="id_user">
+							<?php foreach($users as $user) { ?>
+							<option value="<?php echo $user->id_user ?>"><?php echo $user->lastname . ' ' . $user->firstname; ?></option>
+							<?php } ?>
+						  </select>
+						</div>
+					
+						<div class="form-group">
+						  <label for="PIN">PIN</label>
+						  <input type="password" class="form-control" id="PIN" name="PIN" value="<?php echo $pin; ?>">
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group">
+						  <label>Status</label>
+						  <select class="form-control" name="blocked">
+							<option value="0">Activé</option>
+							<option value="1">Bloqué</option>
+						  </select>
+						</div>
+						<div class="form-group">
+						<label>Envoyer un mail</label>
+						 <div class="input-group">
+							
+							<span class="input-group-addon">
+							  <input type="checkbox">
+							</span>
+							<input type="text" id="email" name="email" class="form-control">
+						  </div><!-- /input-group -->
+						 </div>
+					</div>
                   </div><!-- /.box-body -->
                   <div class="box-footer">
                     <button type="submit" class="btn btn-primary">Submit</button>
                   </div>
                 </form>
               </div>
-            </div><!--/.col (left) -->
+            <!--/.col (left) -->
             <!-- right column -->
-            
-		   </div>
-         
-		 
-		 
+            	 
         </section><!-- /.content -->
       </div><!-- /.content-wrapper -->
-      <footer class="main-footer">
-        <div class="pull-right hidden-xs">
-          <b>Version</b> 1.0.0
-        </div>
-        <strong>Copyright &copy; 2016 <a href="http://www.perenco.com">Perenco</a>.</strong> All rights reserved.
-      </footer>
-
-      <!-- Control Sidebar -->
-      <aside class="control-sidebar control-sidebar-dark">
-        <!-- Create the tabs -->
-        <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-          <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-          <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-        </ul>
-        <!-- Tab panes -->
-        <div class="tab-content">
-          <!-- Home tab content -->
-          <div class="tab-pane" id="control-sidebar-home-tab">
-            <h3 class="control-sidebar-heading">Recent Activity</h3>
-            <ul class="control-sidebar-menu">
-              <li>
-                <a href="javascript::;">
-                  <i class="menu-icon fa fa-birthday-cake bg-red"></i>
-                  <div class="menu-info">
-                    <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-                    <p>Will be 23 on April 24th</p>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="javascript::;">
-                  <i class="menu-icon fa fa-user bg-yellow"></i>
-                  <div class="menu-info">
-                    <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
-                    <p>New phone +1(800)555-1234</p>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="javascript::;">
-                  <i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
-                  <div class="menu-info">
-                    <h4 class="control-sidebar-subheading">Nora Joined Mailing List</h4>
-                    <p>nora@example.com</p>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="javascript::;">
-                  <i class="menu-icon fa fa-file-code-o bg-green"></i>
-                  <div class="menu-info">
-                    <h4 class="control-sidebar-subheading">Cron Job 254 Executed</h4>
-                    <p>Execution time 5 seconds</p>
-                  </div>
-                </a>
-              </li>
-            </ul><!-- /.control-sidebar-menu -->
-
-            <h3 class="control-sidebar-heading">Tasks Progress</h3>
-            <ul class="control-sidebar-menu">
-              <li>
-                <a href="javascript::;">
-                  <h4 class="control-sidebar-subheading">
-                    Custom Template Design
-                    <span class="label label-danger pull-right">70%</span>
-                  </h4>
-                  <div class="progress progress-xxs">
-                    <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="javascript::;">
-                  <h4 class="control-sidebar-subheading">
-                    Update Resume
-                    <span class="label label-success pull-right">95%</span>
-                  </h4>
-                  <div class="progress progress-xxs">
-                    <div class="progress-bar progress-bar-success" style="width: 95%"></div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="javascript::;">
-                  <h4 class="control-sidebar-subheading">
-                    Laravel Integration
-                    <span class="label label-warning pull-right">50%</span>
-                  </h4>
-                  <div class="progress progress-xxs">
-                    <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="javascript::;">
-                  <h4 class="control-sidebar-subheading">
-                    Back End Framework
-                    <span class="label label-primary pull-right">68%</span>
-                  </h4>
-                  <div class="progress progress-xxs">
-                    <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
-                  </div>
-                </a>
-              </li>
-            </ul><!-- /.control-sidebar-menu -->
-
-          </div><!-- /.tab-pane -->
-          <!-- Stats tab content -->
-          <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div><!-- /.tab-pane -->
-          <!-- Settings tab content -->
-          <div class="tab-pane" id="control-sidebar-settings-tab">
-            <form method="post">
-              <h3 class="control-sidebar-heading">General Settings</h3>
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Report panel usage
-                  <input type="checkbox" class="pull-right" checked>
-                </label>
-                <p>
-                  Some information about this general settings option
-                </p>
-              </div><!-- /.form-group -->
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Allow mail redirect
-                  <input type="checkbox" class="pull-right" checked>
-                </label>
-                <p>
-                  Other sets of options are available
-                </p>
-              </div><!-- /.form-group -->
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Expose author name in posts
-                  <input type="checkbox" class="pull-right" checked>
-                </label>
-                <p>
-                  Allow the user to show his name in blog posts
-                </p>
-              </div><!-- /.form-group -->
-
-              <h3 class="control-sidebar-heading">Chat Settings</h3>
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Show me as online
-                  <input type="checkbox" class="pull-right" checked>
-                </label>
-              </div><!-- /.form-group -->
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Turn off notifications
-                  <input type="checkbox" class="pull-right">
-                </label>
-              </div><!-- /.form-group -->
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Delete chat history
-                  <a href="javascript::;" class="text-red pull-right"><i class="fa fa-trash-o"></i></a>
-                </label>
-              </div><!-- /.form-group -->
-            </form>
-          </div><!-- /.tab-pane -->
-        </div>
-      </aside><!-- /.control-sidebar -->
-      <!-- Add the sidebar's background. This div must be placed
-           immediately after the control sidebar -->
-      <div class="control-sidebar-bg"></div>
-    </div><!-- ./wrapper -->
-
-   <!-- jQuery 2.1.4 -->
-   <script src="<?php echo base_url(); ?>assets/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-    <!-- Bootstrap 3.3.5 -->
-    <script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
-    <!-- FastClick -->
-    <script src="<?php echo base_url(); ?>assets/plugins/fastclick/fastclick.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="<?php echo base_url(); ?>assets/dist/js/app.min.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="<?php echo base_url(); ?>assets/dist/js/demo.js"></script>
-   
-  </body>
-</html>
+      <?php include ('inc/footer.php'); ?>
+<script>
+	$('#id_user').on('change', function() {
+	  // alert( this.value ); // or $(this).val()
+	  var server="<?php echo base_url(); ?>index.php/account/get_email/"+this.value+"?";
+	  $.ajax({
+			url: server+"&callback=?",
+			data: "place=client1",
+			type: 'GET',
+			success: function (resp) { 
+			 //alert( resp );
+				$('#email').val(resp);	
+			},
+			error: function(e) {
+				//alert('Error: '+e);
+			}  
+		});
+	});
+     
+</script>
