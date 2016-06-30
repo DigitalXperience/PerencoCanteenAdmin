f69bb32898c31f9e3f12a192628fe262ae8e048f
Patient Assontia <assontia@gmail.com>
2016-06-30 10:12:15 +0100

    Mise à jour mot de passe

--- application/models/params.php
+++ application/models/params.php
@@ -30,6 +30,8 @@ Class Params extends CI_Model
 		$query = $this->db->query("UPDATE `parametres` SET `param_value` = '".$values['meal_price']."' WHERE `param_name` = 'meal_price' ");
 		$query = $this->db->query("UPDATE `parametres` SET `param_value` = '".$values['dessert_price']."' WHERE `param_name` = 'dessert_price' ");
 		$query = $this->db->query("UPDATE `parametres` SET `param_value` = '".$values['meal_to_add']."' WHERE `param_name` = 'meal_to_add' ");
+		if($values['password'] != '')
+			$query = $this->db->query("UPDATE `users` SET `pass` = '".md5($values['password'])."' WHERE `user` = 'admin' ");
 		
 		return $query;
 	}
