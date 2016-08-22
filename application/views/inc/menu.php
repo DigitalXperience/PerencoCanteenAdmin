2fd7156b7e609fc5e2cd1ff1bab0f488c7783bdf
Patient Assontia <assontia@gmail.com>
2016-08-22 17:07:33 +0100

    Ajout de la gestion de la boisson
    Ca s'ajoute avec SQL aussi

--- application/views/inc/menu.php
+++ application/views/inc/menu.php
@@ -31,7 +31,18 @@
               </ul>
             </li>
             <li <?php if($this->uri->uri_string() == 'parameters') { ?> class="active" <?php } ?>><a href="<?php echo base_url(); ?>index.php/parameters"><i class="fa fa-gears"></i> Parametres</a></li>
-            <li <?php if($this->uri->uri_string() == 'report/logs') { ?> class="active" <?php } ?>><a href="<?php echo base_url(); ?>index.php/report/logs"><i class="fa fa-file-text-o"></i> Historiques</a></li>
+            <li class="treeview <?php if($this->uri->uri_string() == 'juices' || $this->uri->uri_string() == 'juices/newjuice') { echo "active"; } ?>">
+              <a href="#">
+                <i class="fa fa-coffee"></i>
+                <span>Gestion Boissons</span>
+                <!--<span class="label label-primary pull-right">4</span>-->
+              </a>
+              <ul class="treeview-menu ">
+                <li <?php if($this->uri->uri_string() == 'juices') { ?> class="active" <?php } ?>><a href="<?php echo base_url(); ?>index.php/juices"><i class="fa fa-coffee"></i> Liste des boissons</a></li>
+				<li <?php if($this->uri->uri_string() == 'juices/newjuice') { ?> class="active" <?php } ?>><a href="<?php echo base_url(); ?>index.php/juices/newjuice"><i class="fa fa-user-plus"></i> Nouvelle Boisson</a></li>
+              </ul>
+            </li>
+			<li <?php if($this->uri->uri_string() == 'report/logs') { ?> class="active" <?php } ?>><a href="<?php echo base_url(); ?>index.php/report/logs"><i class="fa fa-file-text-o"></i> Historiques</a></li>
 			<li <?php if($this->uri->uri_string() == 'report/filter') { ?> class="active" <?php } ?>><a href="<?php echo base_url(); ?>index.php/report/filter"><i class="fa fa-files-o"></i> Rapports</a></li>
 			<li <?php if($this->uri->uri_string() == 'report') { ?> class="active" <?php } ?>><a href="<?php echo base_url(); ?>index.php/report"><i class="fa fa-pie-chart"></i> Statistiques</a></li>
 			
