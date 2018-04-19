0504e23ab4c373d1e2b662d31ec580d08b2a4bc7
Patient Assontia <assontia@gmail.com>
2018-04-19 11:27:50 +0100

    Mise à jour

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 1.0.0
+      <b>Version</b> 1.0.1
     </div>
     <strong>Copyright &copy; 2016 <a href="http://www.perenco.com">Perenco</a>.</strong> All rights reserved.
 </footer>
